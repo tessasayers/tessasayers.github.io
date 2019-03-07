1a5c5fca4d7e37b572afe2becac217ab60362ffd
tessa sayears <tessa.sayers@pitt.edu>
2019-03-07 13:30:46 -0500

    1:30 3/7 fonts work, tried a pop-up

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
-$('.image-link').magnificPopup({type:'image'});
+
+
+ $(document).ready(function() {
+ $('.image-link').magnificPopup({type:'image'});
+      closeOnBgClick: true,closeOnContentClick:true
+       // other options
+   });
+}
 });
