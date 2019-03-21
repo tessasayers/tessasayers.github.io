8babbe7bfb5451d361cca433463bb2fb31de754a
tessa sayears <tessa.sayers@pitt.edu>
2019-03-21 13:02:35 -0400

    updated on 3/21 with new popup

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
 
-
- $(document).ready(function() {
-   $('.image-link').magnificPopup({
-    items: {
-      src: 'brycesignedhere.png'
-    },
-    type: 'image' // this is default type
-});
-
-// other options
-
-});
